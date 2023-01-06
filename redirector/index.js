c8f16783a97e6748b981ac130466a465109c28f0
Florian2807 <gamerflo3.3@gmail.com>
2023-01-06 19:26:08 +0100

    hugest commit i've ever did (hopefully everything works) clueless

---
+++ redirector/index.js
@@ -0,0 +1,184 @@
+const http = require('http')
+const got = require('got')
+const express = require('express')
+const config = require('./config.json')
+const loggedChannels = {}
+
+const request = require('request');
+const app = express()
+
+app.get('/instances/', (req, res) => {
+    res.send(loggedChannels)
+})
+app.get('/channels', (req, res) => {
+    res.set("Access-Control-Allow-Origin", "*")
+    res.send({channels: getAllChannels().sort((a, b) => a.name.localeCompare(b.name))})
+})
+
+app.get('/list', (req, res) => {
+    res.set("Access-Control-Allow-Origin", "*")
+    const channel = /[?&/]channel[=/]([a-zA-Z_0-9]+)/.exec(req.originalUrl)?.[1]
+    const justlogDomain = getJustlogsDomain("name", channel)
+    req.pipe(request(`${justlogDomain}/${req.url}`)).pipe(res);
+})
+
+app.get('/channel/:channelName/user/:userName*', (req, res) => {
+    res.set("Access-Control-Allow-Origin", "*")
+    requestChannelAndUser(req, res)
+})
+app.get('/channel/:channelName/userid/:userName*', (req, res) => {
+    res.set("Access-Control-Allow-Origin", "*")
+    requestChannelAndUser(req, res)
+})
+app.get('/channelid/:channelName/userid/:userName*', (req, res) => {
+    res.set("Access-Control-Allow-Origin", "*")
+    requestChannelAndUser(req, res)
+})
+
+app.get('/channelid/:channelName*', (req, res) => {
+    res.set("Access-Control-Allow-Origin", "*")
+    requestChannel(req, res)
+})
+app.get('/channel/:channelName*', (req, res) => {
+    res.set("Access-Control-Allow-Origin", "*")
+    requestChannel(req, res)
+})
+app.get('/channelid/:channelName*', (req, res) => {
+    res.set("Access-Control-Allow-Origin", "*")
+    requestChannel(req, res)
+})
+
+
+function requestChannel(req, res) {
+
+    const channel = req.params.channelName
+
+    let justlogDomain
+    if (!channel) {
+        console.log("no channel specified")
+        res.sendStatus(404)
+        return
+    }
+
+    const channelRegex = /\/([a-zA-Z_0-9]+)\/([a-zA-Z_0-9]+)\/([0-9]+)\/([0-9]+)\/([0-9]+)/
+
+    const regexTest = channelRegex.exec(req.url)
+    if (!regexTest) {
+        res.redirect(`${req.url}/${new Date().getFullYear()}/${new Date().getMonth() + 1}/${new Date().getDate()}`)
+    }
+
+    const urlPath = req.url.split('?')[0].split('/').filter(i => i !== '')
+    switch (urlPath[0]?.toLowerCase()) {
+        case 'channel':
+            justlogDomain = getJustlogsDomain("name", channel)
+            break
+        case 'channelid':
+            justlogDomain = getJustlogsDomain("id", channel)
+    }
+
+    if (!justlogDomain) {
+        console.log('404 Channel not found')
+        // res.sendStatus(404)
+        return
+    }
+    const requestUrl = `${justlogDomain}/${req.originalUrl}`
+    req.pipe(request(requestUrl)).pipe(res);
+    res.url = requestUrl
+}
+
+function requestChannelAndUser(req, res) {
+
+    const channel = req.params.channelName
+
+    let justlogDomain
+    if (!channel) {
+        console.log("no channel specified")
+        res.sendStatus(404)
+        return
+    }
+
+    const channelRegex = /\/([a-zA-Z_0-9]+)\/([a-zA-Z_0-9]+)\/([0-9]+)\/([0-9]+)/
+
+    const regexTest = channelRegex.exec(req.url)
+    if (!regexTest) {
+        res.redirect(`${req.url}/${new Date().getFullYear()}/${new Date().getMonth() + 1}`)
+    }
+
+    const urlPath = req.url.split('?')[0].split('/').filter(i => i !== '')
+    switch (urlPath[0]?.toLowerCase()) {
+        case 'channel':
+            justlogDomain = getJustlogsDomain("name", channel)
+            break
+        case 'channelid':
+            justlogDomain = getJustlogsDomain("id", channel)
+    }
+    if (!justlogDomain) {
+        console.log('404 Channel not found')
+        // res.sendStatus(404)
+        return
+    }
+    const requestUrl = `${justlogDomain}/${req.originalUrl}`
+    req.pipe(request(requestUrl)).pipe(res);
+    res.url = requestUrl
+}
+
+
+
+const server = http.createServer(app)
+const date = new Intl.DateTimeFormat("de-de", {
+    dateStyle: "medium",
+    timeStyle: "medium",
+}).format(new Date());
+server.listen(config.port, async () => {
+    console.log(`${date}: Server listening on port ${config.port}`)
+
+    await fetchLoggedChannels()
+})
+
+async function fetchLoggedChannels() {
+    for (const justlogInstance in config.domains) {
+        try {
+            const {channels} = await got(`${config.domains[justlogInstance]}/channels`, {retry: {limit: 2}}).json();
+            loggedChannels[justlogInstance] = channels.map(i => {
+                if (!Object.values(loggedChannels).flat().map(c => {
+                    return c?.name
+                }).includes(i.name)) {
+                    return {userID: i.userID, name: i.name}
+                } else {
+                    return undefined
+                }
+            })
+            loggedChannels[justlogInstance] = loggedChannels[justlogInstance].filter(i => i !== undefined)
+        } catch (e) {
+            const date = new Intl.DateTimeFormat("de-de", {
+                dateStyle: "medium",
+                timeStyle: "medium",
+            }).format(new Date());
+            console.warn(`${date} ${justlogInstance}: ${e}`)
+        }
+    }
+}
+
+function getJustlogsDomain(source, channel) {
+    if (source === "name") {
+        const justlogInstance = Object.keys(config.domains).find(justlogInstance => loggedChannels[justlogInstance]?.map(c => c.name).includes(channel))
+        return config.domains[justlogInstance]
+    } else if (source === "id") {
+        const justlogInstance = Object.keys(config.domains).find(justlogInstance => loggedChannels[justlogInstance]?.map(c => c.userID).includes(channel))
+        return config.domains[justlogInstance]
+    }
+}
+
+function getAllChannels() {
+    let allChannels = [];
+    Object.keys(loggedChannels).forEach(instances => {
+        loggedChannels[instances].forEach(channel => {
+            allChannels.push(channel);
+        })
+    })
+    return allChannels
+}
+
+setInterval(async () => {
+    await fetchLoggedChannels()
+}, 600000)
---
+++ web/src/store.tsx
@@ -0,0 +1,146 @@
+import { createContext, useState } from "react";
+import { QueryClient } from 'react-query';
+import { useLocalStorage } from "./hooks/useLocalStorage";
+export interface Settings {
+    showEmotes: Setting,
+    showName: Setting,
+    showTimestamp: Setting,
+    twitchChatMode: Setting,
+    newOnBottom: Setting,
+}
+
+
+export interface Setting {
+    displayName: string,
+    value: boolean,
+}
+
+export interface State {
+    settings: Settings,
+    queryClient: QueryClient,
+    apiBaseUrl: string,
+    currentChannel: string | null,
+    currentUsername: string | null,
+    error: boolean,
+    activeSearchField: HTMLInputElement | null,
+    showSwagger: boolean,
+    showOptout: boolean,
+}
+
+export type Action = Record<string, unknown>;
+const url = new URL(window.location.href);
+const defaultContext = {
+    state: {
+        queryClient: new QueryClient(),
+        apiBaseUrl: "http://localhost:5001",
+        settings: {
+            showEmotes: {
+                displayName: "Show Emotes",
+                value: true,
+            },
+            showName: {
+                displayName: "Show Name",
+                value: true,
+            },
+            showTimestamp: {
+                displayName: "Show Timestamp",
+                value: true,
+            },
+            twitchChatMode: {
+                displayName: "Twitch Chat Mode",
+                value: false,
+            },
+            newOnBottom: {
+                displayName: "Newest messages on bottom",
+                value: false,
+            },
+        },
+        currentChannel: url.searchParams.get("channel"),
+        currentUsername: url.searchParams.get("username"),
+        showSwagger: url.searchParams.has("swagger"),
+        showOptout: url.searchParams.has("optout"),
+        error: false,
+    } as State,
+    setState: (state: State) => { },
+    setCurrents: (currentChannel: string | null = null, currentUsername: string | null = null) => { },
+    setSettings: (newSettings: Settings) => { },
+    setShowSwagger: (show: boolean) => { },
+    setShowOptout: (show: boolean) => { },
+};
+
+const store = createContext(defaultContext);
+const { Provider } = store;
+
+const StateProvider = ({ children }: { children: JSX.Element }): JSX.Element => {
+
+    const [settings, setSettingsStorage] = useLocalStorage("justlog:settings", defaultContext.state.settings);
+    const [state, setState] = useState({ ...defaultContext.state, settings });
+
+    const setShowSwagger = (show: boolean) => {
+        const url = new URL(window.location.href);
+
+        if (show) {
+            url.searchParams.set("swagger", "")
+            url.searchParams.delete("optout");
+        } else {
+            url.searchParams.delete("swagger");
+        }
+
+        window.history.replaceState({}, "justlog", url.toString());
+
+        setState({ ...state, showSwagger: show, showOptout: false })
+    }
+
+    const setShowOptout = (show: boolean) => {
+        const url = new URL(window.location.href);
+
+        if (show) {
+            url.searchParams.set("optout", "");
+            url.searchParams.delete("swagger");
+        } else {
+            url.searchParams.delete("optout");
+        }
+
+        window.history.replaceState({}, "justlog", url.toString());
+
+        setState({ ...state, showOptout: show, showSwagger: false })
+    }
+
+    const setSettings = (newSettings: Settings) => {
+        for (const key of Object.keys(newSettings)) {
+            if (typeof (defaultContext.state.settings as unknown as Record<string, Setting>)[key] === "undefined") {
+                delete (newSettings as unknown as Record<string, Setting>)[key];
+            }
+        }
+
+        state.queryClient.removeQueries("log");
+
+        setSettingsStorage(newSettings);
+        setState({ ...state, settings: newSettings });
+    }
+
+    const setCurrents = (currentChannel: string | null = null, currentUsername: string | null = null) => {
+        currentChannel = currentChannel?.toLowerCase().trim() ?? null;
+        currentUsername = currentUsername?.toLowerCase().trim() ?? null;
+
+        setState({ ...state, currentChannel, currentUsername, error: false });
+
+        const url = new URL(window.location.href);
+        if (currentChannel) {
+            url.searchParams.set("channel", currentChannel);
+        }
+        if (currentUsername) {
+            url.searchParams.set("username", currentUsername);
+        }
+
+        window.history.replaceState({}, "justlog", url.toString());
+    }
+
+    return <Provider value={{ state, setState, setSettings, setCurrents, setShowSwagger, setShowOptout }}>{children}</Provider>;
+};
+
+export { store, StateProvider };
+
+export const QueryDefaults = {
+    staleTime: 5 * 10 * 1000,
+};
